2997dd3f8596e06e1249f9b21c069668a5a79f1b
akm <akm2000@gmail.com>
2020-04-26 00:20:43 +0900

    3章の練習問題のコードを追加

---
+++ chapter-3/index.ts
@@ -0,0 +1,20 @@
+let a = 1042
+let b = 'apples and oranges'
+const c = 'pineapples'
+let d = [true, true, true]
+let e = { type: 'ficus' }
+let f = [1, false]
+const g = [3]
+let h = null
+
+let i: 3 = 3
+i = 4
+
+let j = [1, 2, 3]
+j.push(4)
+j.push('5')
+
+let k: never = 4
+
+let l: unknown = 4
+let m = l * 2
