9e07ce982991f809d333cc52190c8a9b0ec7f279
akm <akm2000@gmail.com>
2020-04-26 11:21:22 +0900

    4.1.6 Symbol.iterator

---
+++ chapter-4/iterator.ts
@@ -0,0 +1,19 @@
+let numbers = {
+    *[Symbol.iterator]() {
+        for (let n = 1; n < 10; n++) {
+            yield n
+        }
+    }
+}
+
+for (let a of numbers) {
+    console.log(a)
+}
+
+const allNumbers = [...numbers]
+console.log(allNumbers)
+
+let [one, two, ...rest] = numbers
+console.log(one)
+console.log(two)
+console.log(rest)
