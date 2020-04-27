f40956e2c14a5993a2c0eb85917f77c3cd40426b
akm <akm2000@gmail.com>
2020-04-27 20:53:51 +0900

    5.11.1 のコードを写経

---
+++ chapter-5/exercise.ts
@@ -0,0 +1,50 @@
+// 1. クラスとインターフェースの違いは何でしょう？
+
+// クラスはインスタンスを作れるがインターフェースは作れない。
+// インターフェースはJavaScriptのコードを発行しない。クラスはする。
+
+// 2. クラスのコンストラクターをprivateではなくprotectedと指定すると何が起こるでしょうか？
+
+class MessageQueue {
+    protected constructor(private messages: string[]) { }
+}
+
+// privateの状態で new MessageQueue([]) とすると、以下のエラー
+// Constructor of class 'MessageQueue' is private and only accessible within the class declaration. [2673]
+//
+// class Foo extends MessageQueue { } という継承するクラスを定義すると以下のエラー
+// Cannot extend a class 'MessageQueue'. Class constructor is marked as private. [2675]
+
+// protectedに変更し new MessageQueue([]) とすると、以下のエラー
+// Constructor of class 'MessageQueue' is private and only accessible within the class declaration. [2673]
+//
+// しかし、class Foo extends MessageQueue { } という継承するクラスを定義してもエラーにならない。
+
+
+// 3. 5.11.1 ファクトリーパターンで作成した実装を拡張して、抽象化を多少犠牲にしてでも、より安全にしてください。
+
+type Shoe = {
+    purpose: string
+}
+
+class BalletFlat implements Shoe {
+    purpose = 'dancing'
+}
+
+class Boot implements Shoe {
+    purpose = 'woodcutting'
+}
+
+class Sneaker implements Shoe {
+    purpose = 'walking'
+}
+
+let Shoe = {
+    create(type: 'balletFlat' | 'boot' | 'sneaker'): Shoe {
+        switch (type) {
+            case 'balletFlat': return new BalletFlat
+            case 'boot': return new Boot
+            case 'sneaker': return new Sneaker
+        }
+    }
+}
