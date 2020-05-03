5dc9a61bc2718af00acfc90448dca2fdbb6d49f4
akm <akm2000@gmail.com>
2020-05-03 15:26:19 +0900

    6.10.4の変更対象の6.6.3の例を追加

---
+++ chapter-6/exercise4.ts
@@ -0,0 +1,10 @@
+// 4. 明確な割当アサーションを使わないように、(「6.6.3 明確な割り当てアサーション」の）例を書き直してください
+
+let userId!: string
+fetchUser()
+
+userId.toUpperCase()
+
+function fetchUser() {
+    userId = globalCache.get('userId')
+}
