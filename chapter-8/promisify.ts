978bf3bad2a4f6bf9f89c4bb776c01911c06babe
akm <akm2000@gmail.com>
2020-05-05 20:56:24 +0900

    Promiseの実装を諦めて既存の実装を使うように変更
    あとちょっとな気がするけど、全然わからないので。

--- chapter-8/promisify.ts
+++ chapter-8/promisify.ts
@@ -1,64 +1,64 @@
 import { readFile } from 'fs'
 
-type Executor<T> = (
-    resolve: (result: T) => void,
-    reject: (error: unknown) => void
-) => void
+// type Executor<T> = (
+//     resolve: (result: T) => void,
+//     reject: (error: unknown) => void
+// ) => void
 
-class Promise<T> {
-    private result?: T
-    private error?: unknown
+// class Promise<T> {
+//     private result?: T
+//     private error?: unknown
 
-    constructor(f: Executor<T>) {
-        f(
-            (r: T) => { this.result = r },
-            (e: unknown) => { this.error = e },
-        )
-    }
+//     constructor(f: Executor<T>) {
+//         f(
+//             (r: T) => { this.result = r },
+//             (e: unknown) => { this.error = e },
+//         )
+//     }
 
-    then<U>(g: (result: T) => Promise<U> | U): Promise<U> {
-        if (this.result) {
-            let r = g(this.result)
-            if (r instanceof Promise) {
-                return r
-            }
-            return new Promise(
-                (resolve: (result: U) => void, _: (error: never) => void) => {
-                    resolve(r as U)
-                }
-            )
-        }
-        return new Promise(
-            (_: (result: never) => void, reject: (error: unknown) => void) => {
-                reject(this.error)
-            }
-        )
-    }
+//     then<U>(g: (result: T) => Promise<U> | U): Promise<U> {
+//         if (this.result) {
+//             let r = g(this.result)
+//             if (r instanceof Promise) {
+//                 return r
+//             }
+//             return new Promise(
+//                 (resolve: (result: U) => void, _: (error: never) => void) => {
+//                     resolve(r as U)
+//                 }
+//             )
+//         }
+//         return new Promise(
+//             (_: (result: never) => void, reject: (error: unknown) => void) => {
+//                 reject(this.error)
+//             }
+//         )
+//     }
 
-    catch<U>(g: (error: unknown) => Promise<U> | U): Promise<U> {
-        if (this.error) {
-            const r = g(this.error)
-            if (r instanceof Promise) {
-                return r
-            }
-            return new Promise(
-                (_: (result: never) => void, reject: (error: unknown) => void) => {
-                    reject(this.error)
-                }
-            )
-        }
+//     catch<U>(g: (error: unknown) => Promise<U> | U): Promise<U> {
+//         if (this.error) {
+//             const r = g(this.error)
+//             if (r instanceof Promise) {
+//                 return r
+//             }
+//             return new Promise(
+//                 (_: (result: never) => void, reject: (error: unknown) => void) => {
+//                     reject(this.error)
+//                 }
+//             )
+//         }
 
-        return new Promise(
-            (resolve: (result: U) => void, _: (error: never) => void) => {
-                if (this.result) {
-                    // resolveに渡す引数の型Uに型Tのthis.resultを渡せない・・・
-                    // catchで返すのは、Promise<T>で良い気がするんだけど・・・
-                    resolve(this.result)
-                }
-            }
-        )
-    }
-}
+//         return new Promise(
+//             (resolve: (result: U) => void, _: (error: never) => void) => {
+//                 if (this.result) {
+//                     // resolveに渡す引数の型Uに型Tのthis.resultを渡せない・・・
+//                     // catchで返すのは、Promise<T>で良い気がするんだけど・・・
+//                     resolve(this.result)
+//                 }
+//             }
+//         )
+//     }
+// }
 
 function promisify<T, U>(f: (arg: T, callback: (e: Error | null, data: U) => void) => void) {
     return (arg: T) => {
